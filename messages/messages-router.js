3fcd726af1b1247746d321af13eeb372f60731ce
mirage <mirage.kamran.pkt@gmail.com>
2020-06-16 18:39:06 -0700

    guided project updated

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -1,26 +1,91 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require("express");
+const db = require("../data/config");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+    try {
+        // translates to SELECT * FROM "messages";
+        const messages = await db.select("*").from("messages");
 
-})
+        res.json(messages);
+    } catch (err) {
+        next(err);
+    }
+});
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
+    try {
+        // SELECT * FROM "messages" WHERE "id" = ? LIMIT 1;
+        // destructure the result since we only care about the first index of the array
+        const message = await db
+            .select("*")
+            .from("messages")
+            .where("id", req.params.id)
+            // make sure we're only getting a single result,
+            // since we're destructuring the array above
+            .limit(1);
 
-})
+        res.json(message);
+    } catch (err) {
+        next(err);
+    }
+});
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+    try {
+        const payload = {
+            // the database will automatically generate the ID and dates
+            title: req.body.title,
+            contents: req.body.contents
+        };
 
-})
+        // translates to INSERT INTO "messages" ("title", "contents") VALUES (?, ?);
+        const [messageID] = await db.insert(payload).into("messages");
+        // callig `.first()` is doing the same thing as `.limit(1) and destructuring the result
+        const message = await db
+            .first("*")
+            .from("messages")
+            .where("id", messageID);
 
-router.put("/:id", (req, res, next) => {
+        res.status(201).json(message);
+    } catch (err) {
+        next(err);
+    }
+});
 
-})
+router.put("/:id", async (req, res, next) => {
+    try {
+        const payload = {
+            // the database will automatically generate the ID and dates
+            title: req.body.title,
+            contents: req.body.contents
+        };
 
-router.delete("/:id", (req, res, next) => {
+        // translates to `UPDATE "messages" SET ? = ? WHERE "id" = ?`;
+        await db("messages").update(payload).where("id", req.params.id);
+        // callig `.first()` is doing the same thing as `.limit(1) and destructuring the result
+        const message = await db
+            .first("*")
+            .from("messages")
+            .where("id", req.params.id);
 
-})
+        res.json(message);
+    } catch (err) {
+        next(err);
+    }
+});
 
-module.exports = router
+router.delete("/:id", async (req, res, next) => {
+    try {
+        // translates to `DELET FROM "messages" WHERE "id" = ?;`
+        await db("messages").where("id", req.params.id).del();
+        // since we no longer have a resource to return,
+        // just send a 204 which means "success, but no response data is being sent back"
+        res.status(204).end();
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
